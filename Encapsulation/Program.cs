c7039b4832de89fd385f5c513a42499dd0b85fdd
sonam <sonamsahu199518@gmail.com>
2022-04-19 21:35:56 +0530

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Order
+
+    {
+        public int i = 20;
+
+        //Encapsulation is the concept of wrapping data into a single unit. It collects data members and
+        //member functions into a single unit called class. The purpose of encapsulation is to prevent alteration of data from outside.
+        //Encapsulation provide the securites to data from outsied the class every class example of Encapsulation but inside the class data member or function is Complsery
+        //Encapsulation are two type 1.Normal Encapsulation:- if each and every variable is declared as punlic then it is clld Normal Encapsulation.
+        //2. Tigtly Encapsulation:-if each and every variable is decleared as a private it is cld tightly Encapsulation.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
